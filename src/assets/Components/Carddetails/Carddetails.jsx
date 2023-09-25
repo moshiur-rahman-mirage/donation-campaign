edfdfaffef280bb90b6b1b6241cc4884b9f7aa12
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-09-25 22:22:07 +0600

    donation details overlay done

--- src/assets/Components/Carddetails/Carddetails.jsx
+++ src/assets/Components/Carddetails/Carddetails.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import './Carddetails.css'
 
 const Carddetails = () => {
     const cardsdata = useLoaderData();
@@ -8,15 +8,28 @@ const Carddetails = () => {
     console.log(currentcard)
     return (
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: `url(${currentcard.picture})` }}>
+            {/* <div className="hero min-h-screen" style={{ backgroundImage: `url(${currentcard.picture})` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="">
                     <div className="max-w-md top-2/3 mt-[600px] mr-[900px]">
                         <button className="btn btn-primary">Donate</button>
                     </div>
                 </div>
+            </div> */}
+
+            <div className="image-container">
+                <div >
+                    <img src={currentcard.picture} alt="" />
+                    <div className="overlay">
+                        <button className="overlay-button" style={{background:`${currentcard.text_button_bg_color}`}}>Donate {currentcard.price}</button>
+                    </div>
+                </div>
             </div>
-            <h2 className="text-4xl">{currentcard.description}</h2>
+            <br/>
+            <h2 className="text-4xl">{currentcard.title}</h2>
+            <br/>
+            <h2 className="text-xl">{currentcard.description}</h2>
+            <br/>
         </div>
     );
 };
